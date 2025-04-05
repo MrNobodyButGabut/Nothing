254fbcfe9e6e7db56a157dc0c7afda15710f6b41
Robby Chandra <rochan.130904@gmail.com>
2025-04-06 01:10:56 +0700

    Initial commit: Love letter project

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      animation: {
+        "bounce-custom": "bounce-custom 2s infinite",
+      },
+      keyframes: {
+        "bounce-custom": {
+          "0%, 100%": {
+            transform: "translateY(-15px)",
+            animationTimingFunction: "cubic-bezier(0.8, 0, 1, 1)",
+          },
+          "50%": {
+            transform: "translateY(0)",
+            animationTimingFunction: "cubic-bezier(0, 0, 0.2, 1)",
+          },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
